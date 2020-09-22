9ab9ba20203b160384d3a6a233e59b0ea2b4c6b9
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-09-23 01:57:33 +0530

    Updates

---
+++ precache-manifest.84b7b0e3990817081fd731222216e0a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750448fd511d23b95a1455bbdb6a0d6e",
+    "url": "/fashiony-clothing/index.html"
+  },
+  {
+    "revision": "28c17d5239d4b778e199",
+    "url": "/fashiony-clothing/static/css/main.61e2da81.chunk.css"
+  },
+  {
+    "revision": "067f3a7ba3f97106490c",
+    "url": "/fashiony-clothing/static/js/2.8006b987.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/fashiony-clothing/static/js/2.8006b987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c17d5239d4b778e199",
+    "url": "/fashiony-clothing/static/js/main.9d71c1da.chunk.js"
+  },
+  {
+    "revision": "1fb1ac522507019f5ecd",
+    "url": "/fashiony-clothing/static/js/runtime-main.9274975a.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/fashiony-clothing/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/fashiony-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
